259f997052f2769826a8a39c2f84215172ac49b8
Ravexina <ravexina@gmail.com>
2020-04-24 02:10:39 +0430

    change example from fa to en

--- pwg.py
+++ pwg.py
@@ -5,8 +5,8 @@ import itertools
 def generate(vals="letters"):
     return list(("".join(x) for x in itertools.chain.from_iterable(itertools.permutations(vals,i+1) for i in range(0,len(vals)))))
 
-# Generate all possible words using letters: ا ب ث:
-words = generate("ابث")
+# Generate all possible words using letters: a b c:
+words = generate("abc")
 
 # Print out the words
 for word in words:
