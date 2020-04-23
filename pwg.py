8f48e2e8cbfb47889d281361f60a74e88a871f87
ravexina <ravexina@gmail.com>
2020-04-24 01:55:22 +0430

    Add main script

---
+++ pwg.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import itertools
+
+def generate(vals="letters"):
+    return list(("".join(x) for x in itertools.chain.from_iterable(itertools.permutations(vals,i+1) for i in range(0,len(vals)))))
+
+# Generate all possible words using letters: ا ب ث:
+words = generate("ابث")
+
+# Print out the words
+for word in words:
+    print(word)
